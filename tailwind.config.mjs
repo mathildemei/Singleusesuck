cb7c9824beb0f7c3af5a78dac4976c1f5173e464
Mathilde <maje0014@stud.kea.dk>
2024-10-25 11:40:35 +0200

    test

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,32 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            fontFamily: {
+                primary: ["EB Garamond", "serif"], // Definer primær skrifttype
+                secondary: ["Campton", "sans-serif"], // Definer sekundær skrifttype
+              },
+              fontWeight: {
+                bold: "700", // Definer bold vægt
+                "semi-bold": "600", // Definer semi-bold vægt
+                light: "300", // Definer light vægt
+              },
+              fontSize: {
+                // Desktop størrelser
+                "h1-d": "120px",
+                "h2-d": "44px",
+                "h3-d": "34px",
+                "h4-d": "28px",
+    
+              },
+              colors: {
+                white: "#ffffff",
+                black: "#000000",
+                "dark-graphite": "#1C1E23",
+                "plastic-red": "#FF1054",
+                "green": "#007C79",
+              },
+            },
 	},
 	plugins: [
         require('@tailwindcss/typography'),
